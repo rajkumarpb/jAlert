53795706f27776f809559afbb5d3b73f2ca8878c
Rajkumar <pb.rajkumar@yahoo.com>
2016-02-01 19:08:31 +0530

    Initial Changes Commit!

--- angular-jalert/angular-jalert.js
+++ angular-jalert/angular-jalert.js
@@ -5,20 +5,43 @@
  * License: Dual-licensed under the GNU General Public License and the MIT License
  */
 var CONSTANTS = {
-	ALERT_TITLE : 'Information'
+	ALERT_TITLE : 'Information',
+	CONFIRM_TITLE : 'Confirmation',
+    PROMPT_TITLE : 'Please enter the value'
 };
 var jalertModule = angular.module('jalert')
 jalertModule.constant("CONSTANTS", CONSTANTS);
 jalertModule.directive('jalert', function() {
- return {
+	return {
         restrict: 'A',
+		scope: {
+			callback: '&jcallback'
+		},
         link: function(scope, iElem, iAttrs) {
             iElem.bind("click", function() {
-            	jAlert(iElem.attr("jalert"), CONSTANTS.ALERT_TITLE);
-            });
+				if(iElem.attr("jalert-type")!=undefined){
+					if(iElem.attr("jalert-type")=='confirm'){
+						jConfirm(iElem.attr("jalert-message"), CONSTANTS.CONFIRM_TITLE, function(res) {
+						if(res && iElem.attr("jcallback")!=undefined && iElem.attr("jcallback")!='')
+							scope.callback();
+						});
+					} else if(iElem.attr("jalert-type")=='prompt'){
+                        jPrompt(iElem.attr("jalert-message"), CONSTANTS.PROMPT_TITLE, function(res) {
+						if(res && iElem.attr("jcallback")!=undefined && iElem.attr("jcallback")!='')
+                            scope.callback();
+                        });
+                    };
+				}else{
+                    jAlert(iElem.attr("jalert"), CONSTANTS.APP_NAME_AS_PREFIX, function(res){
+                        if(iElem.attr("jcallback")!=undefined && iElem.attr("jcallback")!='')
+                            scope.callback();
+                    });
+                }
             scope.$on('$destroy', function() {
             	iElem.off();
             });
+        });
+            
         }
     };
 });
